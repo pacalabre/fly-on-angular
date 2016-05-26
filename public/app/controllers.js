d83abf713eb0670b3243ea66ece6ff1cf4870569
Paul <pacalabre@gmail.com>
2016-05-26 16:35:02 -0700

    info pushes to database

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -5,6 +5,30 @@ ctrls.controller('TakeOffCtrl',['$scope', function($scope){
 }])
 
 
-ctrls.controller('InventoryCtrl',['$scope', function($scope){
-  $scope.planes = [232,454,177];
+ctrls.controller('InventoryCtrl',['$scope', 'AirplaneAPI',
+  function($scope, AirplaneAPI){
+  $scope.planes = [];
+
+  AirplaneAPI.query(function success(data){
+    console.log("success = " + data);
+    $scope.planes = data;
+  }, function error(data){
+    console.log("err = " + data)
+  })
+
+  $scope.createPlane = function(manufacturer, model, engines) {
+
+    console.log("args:"+manufacturer + model + engines)
+    AirplaneAPI.save({
+      manufacturer: manufacturer,
+      model: model,
+      engines: engines
+    }, function success(plane) {
+      console.log(plane)
+      $scope.planes.push(plane);
+    }, function error(data) {
+      console.log(data);
+    })
+  }
 }])
+
--- public/app/services.js
+++ public/app/services.js
@@ -1,1 +1,6 @@
-var services = angular.module('AirplaneServices',[]);
+var services = angular.module('AirplaneServices',['ngResource']);
+
+services.factory('AirplaneAPI',['$resource', function($resource) {
+  return $resource('/api/airplanes/:id');
+}])
+
